d27c1709cf6dd040fdd659f4e477c07a0d646734
Xabibulloh-J <xjorayev378@gmail.com>
2024-07-17 16:58:54 +0400

    js code change function

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 // RANDOM O'YINI
 var elForm = document.querySelector(".js-form");
 var elRandomInput = elForm.querySelector(".js-input");
+var elButton = elForm.querySelector(".js-btn");
 var elRandomDesc = document.querySelector(".js-random-desc");
 var elRandomErrorDesc = document.querySelector(".js-random-error-desc");
 var elRefresh = document.querySelector(".js-refresh");
 var randomNumber = Math.round(Math.random() * 100) + 1;
 var userAttempt = 6;
 
-elForm.addEventListener("submit", function(evt) {
-   evt.preventDefault()
-      
+function checkUserInput(evt) {
+   evt.preventDefault();
+   
    if (elRandomInput.value == randomNumber) {
       elRandomDesc.textContent = "Urra, to'g'ri topdingiz";
       elRandomErrorDesc.textContent = "";
+      elRandomInput.disabled = true;
+      elButton.disabled = true;
       elRefresh.textContent = "Yana o'ynab ko'ring"
-   } else if (elRandomInput.value < randomNumber) {
+   }
+   
+   if (elRandomInput.value < randomNumber) {
       elRandomErrorDesc.textContent = "Siz bergan son kichiklik qilyapti!";
       elRandomDesc.textContent = "";
-   } else {
+   } 
+   
+   if(elRandomInput.value > randomNumber) {
       elRandomErrorDesc.textContent = "Siz bergan son kattalik qilyapti!";
       elRandomDesc.textContent = "";
    };
@@ -25,7 +32,11 @@ elForm.addEventListener("submit", function(evt) {
    userAttempt--;
    
    if (userAttempt == 0) {
-      elRandomErrorDesc.textContent = `Afsuski imkoniyatlaringizni tugatdingiz. Topa olmagan soningiz ${randomNumber} edi.`;
+      elRandomErrorDesc.textContent = `Afsuski imkoniyatlaringiz tugadi. Topa olmagan soningiz ${randomNumber} edi.`;
+      elRandomInput.disabled = true;
+      elButton.disabled = true;
       elRefresh.textContent = "Yana urunib ko'ring";
    };
-});
+};
+
+elForm.addEventListener("submit", checkUserInput);
